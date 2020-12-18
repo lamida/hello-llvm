4158f1dedef0295568042b88f68b987810f54d5f
Jon Kartago Lamida <lamidaj@gmail.com>
2020-12-18 09:28:49 +0800

    Apply clang-format

--- hello-cpp/hello-array.cpp
+++ hello-cpp/hello-array.cpp
@@ -1,6 +1,6 @@
 #include <array>
-#include <vector>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -8,7 +8,8 @@ int main() {
   cout << "Input some numbers:";
   int num = 0;
   cin >> num;
-  cout << "First let's create array for compile time size. We will set it to 4" << endl;
+  cout << "First let's create array for compile time size. We will set it to 4"
+       << endl;
   array<int, 4> ar = {
       num, num, num,
       num}; // needed in C++11 after the revision and in C++14 and beyond)
@@ -19,12 +20,12 @@ int main() {
   cout << "First let's create vector for dynamic size array" << endl;
   vector<int> v = vector<int>();
   for (auto i = 0; i < num; i++) {
-      v.push_back(i);
+    v.push_back(i);
   }
 
   for (auto x : v) {
-      cout << x << endl;
+    cout << x << endl;
   }
-  
+
   return 0;
 }

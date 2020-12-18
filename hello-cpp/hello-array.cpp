80022e608a30e879b6a6124dc35b9b2c13979d1d
Jon Kartago Lamida <lamidaj@gmail.com>
2020-12-18 09:28:35 +0800

    Use vector

--- hello-cpp/hello-array.cpp
+++ hello-cpp/hello-array.cpp
@@ -1,4 +1,5 @@
 #include <array>
+#include <vector>
 #include <iostream>
 
 using namespace std;
@@ -7,11 +8,23 @@ int main() {
   cout << "Input some numbers:";
   int num = 0;
   cin >> num;
+  cout << "First let's create array for compile time size. We will set it to 4" << endl;
   array<int, 4> ar = {
       num, num, num,
       num}; // needed in C++11 after the revision and in C++14 and beyond)
   for (const auto &el : ar) {
     cout << el << endl;
   }
+
+  cout << "First let's create vector for dynamic size array" << endl;
+  vector<int> v = vector<int>();
+  for (auto i = 0; i < num; i++) {
+      v.push_back(i);
+  }
+
+  for (auto x : v) {
+      cout << x << endl;
+  }
+  
   return 0;
 }

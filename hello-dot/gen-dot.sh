67003bc38080f2732b042b65e30a2c2ed05649d9
Jon Kartago Lamida <me@lamida.net>
2021-01-14 10:06:18 +0800

    Generate different opt --dot-* operations

---
+++ hello-dot/gen-dot.sh
@@ -0,0 +1,10 @@
+clang -emit-llvm llvm-dot.c -c
+rm -rf dot
+DOTS="callgraph\ncfg\ncfg-only\ndom\ndom-only\npostdom\npostdom-only\nregions\nregions-only\nscops\nscops-only" 
+echo $DOTS| xargs -I{} mkdir -p dot/dot-{}
+for dot in $(ls -d dot/*/); do
+cd $dot
+opt "--${PWD##*/}" ../../llvm-dot.bc
+ls *.dot | xargs -I{} dot -Tpng {} -o {}.png
+cd ../..
+done
---
+++ hello-dot/readme.md
@@ -0,0 +1,3 @@
+# LLVM opt dot* analyser
+
+In this folder we are generating different analyser viewer provided by LLVM. See gen-dot.sh to see how the graph is generated. We are using llvm-dot.c as the source file.

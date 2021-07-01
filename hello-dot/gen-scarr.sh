a74183f0cb3bcf18f615d473834fb48351a7af51
Jon Kartago Lamida <me@lamida.net>
2021-07-01 14:22:39 +0800

    Update using latest checkpoint marker

--- hello-dot/gen-scarr.sh
+++ hello-dot/gen-scarr.sh
@@ -20,7 +20,7 @@ sed -i 's/^attributes/;&/g' $out_title.ll
 
 mkdir -p $out_title/dot-scarr
 cd $out_title/dot-scarr
-/home/lamida/github/llvm/llvm-project/llvm/cmake-build-release/bin/opt ../../$out_title.ll -passes=mycfg,dot-cfg
+/home/lamida/github/llvm/llvm-project/llvm/cmake-build-release/bin/opt ../../$out_title.ll -passes=scarr-cp-marker,dot-cfg
 ls *.dot | xargs -I{} dot -Tpng {} -o {}.png
 # generate png for hidden files too
 ls .*.dot | xargs -I{} dot -Tpng {} -o {}.png
@@ -29,7 +29,7 @@ cd ../..
 
 mkdir -p $out_title/dot-scarr-inline
 cd $out_title/dot-scarr-inline
-/home/lamida/github/llvm/llvm-project/llvm/cmake-build-release/bin/opt ../../$out_title.ll -passes=inliner-wrapper,mycfg,dot-cfg
+/home/lamida/github/llvm/llvm-project/llvm/cmake-build-release/bin/opt ../../$out_title.ll -passes=inliner-wrapper,scarr-cp-marker,dot-cfg
 ls *.dot | xargs -I{} dot -Tpng {} -o {}.png
 # generate png for hidden files too
 ls .*.dot | xargs -I{} dot -Tpng {} -o {}.png
